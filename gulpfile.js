fd83d71e5402e34bfbf4a497686fe8ad3be9aaaa
Mteuahasan <matthieu.lachassagne@hetic.net>
2015-01-27 19:08:21 +0100

    Added gulp and his config

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    notify = require('gulp-notify'),
+    rename = require('gulp-rename'),
+    livereload = require('gulp-livereload'),
+    del = require('del');
+
+gulp.task('styles', function() {
+  return sass('public/styles/')
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('dist/assets/css'))
+    .pipe(notify({ message: 'Styles task complete' }));
+});
+
+gulp.task('clean', function(cb) {
+    del(['dist/assets/css', 'dist/assets/js', 'dist/assets/img'], cb)
+});
+
+gulp.task('default', ['clean'], function() {
+    gulp.start('styles');
+});
+
+gulp.task('watch', function() {
+
+  // Watch .scss files
+  gulp.watch('public/styles/**/*.scss', ['styles']);
+
+  // Create LiveReload server
+  livereload.listen();
+
+  // Watch any files in dist/, reload on change
+  gulp.watch(['dist/**']).on('change', livereload.changed);
+
+});
