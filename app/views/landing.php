d8292ec15236109fc4f66b6f263f98d63ef9b137
Annabelle Ruiz <annabelle.ruiz@hetic.net>
2015-02-16 21:08:44 +0100

    added inté assets

---
+++ app/views/landing.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>Striply</title>
+  <meta name="description" content="">
+  <base href="<?php echo $BASE; ?>/" />
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="./dist/assets/css/main.css">
+
+</head>
+<body>
+  <div class="landing">
+  	<h1 class='logo'>Striply</h1>
+  	<div class="content-landing">
+  		<p class="subtitle">Comics creators have Their place now</p>
+  		<p class="title">Welcome to Striply</p>
+
+  		<a href="#" class="btn-members">
+  			<div class="btn-members--content">Become a cartoonist member</div>
+  			<div class="btn-members--nbr">10 189</div>
+  		</a>
+
+  		<a href="#" class="btn-start">Discover the site</a>
+
+  	</div>
+  </div>
+</body>
+</html>
