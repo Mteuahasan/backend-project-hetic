45ae5775d41cc29bd149a09c369b3894ffdb2808
Mteuahasan <matthieu.lachassagne@hetic.net>
2015-03-06 04:32:59 +0100

    search inte add file

---
+++ app/views/partials/search-popup.php
@@ -0,0 +1,15 @@
+<section id="searchFilter" class="page-filters-search">
+    <div id="pageSearch" class="page-search">
+      <i id="closeSearch" class="flaticon-cancel22"></i>
+      <h1>Search anything by tiping here</h1>
+      <i class="flaticon-information32" title="Category, people or tag"></i>
+      <form action="search/" method="post" id='search-form'>
+          <input type="text" name="name" placeholder="" autocomplete="off" id="search-content"/>
+      </form>
+      <div id="results-container">
+        <div id="results-wrapper">
+
+        </div>
+      </div>
+    </div>
+  </section>
